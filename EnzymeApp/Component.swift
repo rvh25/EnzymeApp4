1c9278066b31664bd52a0690445e0fe0c45458a5
rvh25 <rohainehsu@Rohaines-MacBook-Pro.local>
2015-08-03 23:33:56 -0400

    ea4

--- EnzymeApp/Component.swift
+++ EnzymeApp/Component.swift
@@ -10,39 +10,13 @@ import SpriteKit
 
 enum ComponentType: Int, CustomStringConvertible{
     
-    //case Unknown = 0, Activator, Competitive_Inhibitor, Deactivator, Enzyme, Noncompetitive_Inhibitor, Substrate
+    case Unknown = 0,  Enzyme, Substrate, Activator, Competitive_Inhibitor, Deactivator, Noncompetitive_Inhibitor, Enzyme_with_2_activators_and_substrate, Enzyme_with_Competitive_Inhibitor, Enzyme_with_deactivator, Enzyme_with_Noncompetitive_Inhibitor_and_substrate, Enzyme_with_Noncompetitive_Inhibitor, Enzyme_with_one_activator_and_substrate, Enzyme_with_one_activator, Enzyme_with_two_activators, ESComplex, Product
+    
 
-    
-    /*case Unknown = 0, Activator, Competitive_Inhibitor, Deactivator, Enzyme, Noncompetitive_Inhibitor, Substrate, Enzyme_with_2_activators_and_substrate, Enzyme_with_Competitive_Inhibitor, Enzyme_with_deactivator, Enzyme_with_Noncompetitive_Inhibitor_and_substrate, Enzyme_with_Noncompetitive_Inhibitor, Enzyme_with_one_activator_and_substrate, Enzyme_with_one_activator, Enzyme_with_two_activators,  ESComplex,  Product*/
-    
-    case Unknown = 0,  Enzyme, Substrate,Activator, Competitive_Inhibitor, Deactivator, Noncompetitive_Inhibitor,  Enzyme_with_2_activators_and_substrate, Enzyme_with_Competitive_Inhibitor, Enzyme_with_deactivator, Enzyme_with_Noncompetitive_Inhibitor_and_substrate, Enzyme_with_Noncompetitive_Inhibitor, Enzyme_with_one_activator_and_substrate, Enzyme_with_one_activator, Enzyme_with_two_activators,  ESComplex,  Product
-    
-/*
-    case Unknown = 0, Activator, Competitive_Inhibitor, Deactivator, Enzyme_with_2_activators_and_substrate, Enzyme_with_Competitive_Inhibitor, Enzyme_with_deactivator, Enzyme_with_Noncompetitive_Inhibitor_and_substrate, Enzyme_with_Noncompetitive_Inhibitor, Enzyme_with_one_activator_and_substrate, Enzyme_with_one_activator, Enzyme_with_two_activators, Enzyme, ESComplex, Noncompetitive_Inhibitor, Product, Substrate
-*/
     
     var spriteName: String {
         let spriteNames = [
-        
-            /*"Activator",
-            "Competitive_Inhibitor",
-            "Deactivator",
-            "Enzyme",
-            "Noncompetitive_Inhibitor",
-            "Substrate",
-            "Enzyme_with_2_activators_and_substrate",
-            "Enzyme_with_Competitive_Inhibitor",
-            "Enzyme_with_deactivator",
-            "Enzyme_with_Noncompetitive_Inhibitor_and_substrate",
-            "Enzyme_with_Noncompetitive_Inhibitor",
-            "Enzyme_with_one_activator_and_substrate",
-            "Enzyme_with_one_activator",
-            "Enzyme_with_two_activators",
-            "ESComplex",
-            "Product"
-        ]*/
-        
-        
+     
         "Enzyme",
         "Substrate",
         "Activator",
@@ -62,53 +36,18 @@ enum ComponentType: Int, CustomStringConvertible{
         ]
         
         
-            /*"Activator",
-            "Competitive_Inhibitor",
-            "Deactivator",
-            "Enzyme",
-            "Noncompetitive_Inhibitor",
-            "Substrate"
-        ]*/
-
-/*
-     "Activator",
-        "Competitive_Inhibitor",
-        "Deactivator",
-        "Enzyme_with_2_activators_and_substrate",
-        "Enzyme_with_Competitive_Inhibitor",
-        "Enzyme_with_deactivator",
-        "Enzyme_with_Noncompetitive_Inhibitor_and_substrate",
-        "Enzyme_with_Noncompetitive_Inhibitor",
-        "Enzyme_with_one_activator_and_substrate",
-        "Enzyme_with_one_activator",
-        "Enzyme_with_two_activators",
-        "Enzyme",
-        "ESComplex",
-        "Noncompetitive_Inhibitor",
-        "Product",
-        "Substrate"
-        ]
-*/
-
-        
         return spriteNames[rawValue - 1]
     }
     
     static func random() -> ComponentType {
         return ComponentType(rawValue: Int(arc4random_uniform(2)) + 1)!
         //return ComponentType(rawValue: Int(arc4random_uniform(6)) + 1)!
-        //return ComponentType(rawValue: Int(arc4random_uniform(16)) + 1)!
     }
     
     var description: String {
         return spriteName
     }
 }
-
-//    var highlightedSpriteName: String {
-//        return spriteName + "-Highlighted"
-//    }
-    
 
 
 class Component: CustomStringConvertible, Hashable {
